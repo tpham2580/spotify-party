8e027101527b34f3fc384b50e461c7850afeb406
tpham2580 <tpham2580@gmail.com>
2021-04-20 21:26:04 -0500

    added app react component

--- spotify_party/settings.py
+++ spotify_party/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
